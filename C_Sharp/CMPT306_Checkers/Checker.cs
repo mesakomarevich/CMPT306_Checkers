75cb637d2768d1e2cf4a112d976c98e5590a208b
Mesa Komarevich <mesakom@gmail.com>
2019-11-25 15:37:23 -0600

    Adding fsharp version

---
+++ C_Sharp/CMPT306_Checkers/Checker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace CMPT306_Checkers
+{
+    public class Checker
+    {
+        public int Id { get; set; }
+
+        public Color Color { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public bool King { get; set; }
+
+        public Checker()
+        {
+        }
+
+        public Checker(int newId, Color newColor, int newX, int newY) : this()
+        {
+            Id = newId;
+            Color = newColor;
+            X = newX;
+            Y = newY;
+        }
+    }
+
+    public enum Color
+    {
+        Black = 0,
+        Red = 1
+    }
+}
---
+++ C_Sharp/CMPT306_Checkers/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CMPT306_Checkers
+{
+    public class Tile
+    {
+        public Color Color { get; set; }
+
+        public Checker Checker { get; set; }
+
+        public Tile()
+        {
+        }
+
+        public Tile(Color newColor) : this()
+        {
+            Color = newColor;
+        }
+
+        public Tile(Color newColor, Checker newChecker) : this()
+        {
+            Checker = newChecker;
+        }
+    }
+}
